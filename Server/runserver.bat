fb42527d10dbd2de111c3fd91763b7f86fc66658
GeorgeSedhom0 <george.sedhom2003@gmail.com>
2024-04-25 11:32:08 +0200

    Add @mui/lab and react-virtualized dependencies, and implement sync functionality in Layout component

---
+++ Server/runserver.bat
@@ -0,0 +1,1 @@
+cd env && cd Scripts && activate && cd .. && cd .. && uvicorn main:app --host 0.0.0.0 --workers 4
--- Store/src/Layout.tsx
+++ Store/src/Layout.tsx
@@ -1,9 +1,29 @@
 import { AppBar, Button, Toolbar, Grid } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
 import { ViewContainer } from "./pages/Shared/Utils";
 import { useNavigate } from "react-router-dom";
+import { useCallback, useState } from "react";
+import AlertMessage, { AlertMsg } from "./pages/Shared/AlertMessage";
+import axios from "axios";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
+  const [msg, setMsg] = useState<AlertMsg>({ type: "", text: "" });
+  const [loading, setLoading] = useState(false);
+
   const navigate = useNavigate();
+
+  const sync = useCallback(async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.post("http://localhost:8000/send-sync");
+      console.log(data);
+      setMsg({ type: "success", text: "تمت المزامنة بنجاح" });
+    } catch (e) {
+      setMsg({ type: "error", text: "حدث خطا ما" });
+    }
+    setLoading(false);
+  }, []);
+
   return (
     <>
       <AppBar position="sticky">
@@ -44,17 +64,16 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             >
               تعديل المنتجات
             </Button>
-            <Button
-              onClick={() => {
-                console.log("logout");
-              }}
-            >
+            <LoadingButton loading={loading} onClick={sync}>
               مزامنة
-            </Button>
+            </LoadingButton>
           </Grid>
         </Toolbar>
       </AppBar>
-      <ViewContainer>{children}</ViewContainer>
+      <ViewContainer>
+        <AlertMessage message={msg} setMessage={setMsg} />
+        {children}
+      </ViewContainer>
     </>
   );
 };
